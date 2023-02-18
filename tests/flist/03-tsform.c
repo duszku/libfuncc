c1e754346b6ccd68d817e2c7d18d89b4f9d041a8
duszku <86882994+duszku@users.noreply.github.com>
2023-02-18 10:23:42 +0100

    Tested `flist_map_inplace()`

--- tests/flist/03-tsform.c
+++ tests/flist/03-tsform.c
@@ -27,6 +27,9 @@ square(void *x)
         return ret;
 }
 
+void *retnul(void *ign) { return NULL; }
+void *id(void *x) { return x; }
+
 TestSuite(flist_tsform, .init=setup, .fini=end);
 
 Test(flist_tsform, map)
@@ -44,6 +47,54 @@ Test(flist_tsform, map)
         }
 
         flist_free(&l2, 0);
+}
+
+Test(flist_tsform, map_inplace_change)
+{
+        void    *dat;
+        int      i;
+
+        flist_map_inplace(list, square, 0);
+        for (i = 0; i < A_LEN; ++i) {
+                dat = flist_head_inplace(list);
+                cr_expect_eq(*((int *)dat), i * i);
+                free(dat);
+        }
+}
+
+Test(flist_tsform, map_inplace_sidegen_nul)
+{
+        void    *dat;
+        int      i;
+
+        flist_map_inplace(list, retnul, 0);
+        for (i = 0; i < A_LEN; ++i) {
+                dat = flist_head_inplace(list);
+                cr_expect_eq(dat, arr + i);
+        }
+}
+
+Test(flist_tsform, map_inplace_sidegen_id)
+{
+        void    *dat;
+        int      i;
+
+        flist_map_inplace(list, id, 0);
+        for (i = 0; i < A_LEN; ++i) {
+                dat = flist_head_inplace(list);
+                cr_expect_eq(dat, arr + i);
+        }
+}
+
+Test(flist_tsform, map_inplace_force)
+{
+        int *hp;
+
+        assert((hp = malloc(sizeof(int))) != NULL);
+        *hp = 100;
+
+        list = flist_prepend(list, hp, ELEM_HEAP);
+        flist_map_inplace(list, square, 1);
 }
 
 void
