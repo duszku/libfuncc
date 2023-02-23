d5fb59bfcae5fb50a6b22c364b4924a218c3a984
duszku <86882994+duszku@users.noreply.github.com>
2023-02-23 22:45:58 +0100

    Documentation fix

--- flist.c
+++ flist.c
@@ -62,8 +62,8 @@ struct flist_iter {
         struct       flist_iter *prev;  /**< @brief Previous node */
         void        *data;              /**< @brief Pointer to the data */
 
-        unsigned     call_h : 1;      /**< @brief Call cleanup handler? */
-        unsigned     prot_h : 1;      /**< @brief Call cleanup iff forced? */
+        unsigned     call_h : 1;        /**< @brief Call cleanup handler? */
+        unsigned     prot_h : 1;        /**< @brief Call cleanup iff forced? */
 };
 
 /**
@@ -77,7 +77,7 @@ struct flist_iter {
 struct flist {
         struct       flist_iter *head;  /**< @brief Head of the list */
         struct       flist_iter *tail;  /**< @brief Tail of the list */
-        void       (*cl_hand)(void *); /**< @brief Cleanup handlers */
+        void       (*cl_hand)(void *);  /**< @brief Cleanup handler */
         size_t       len;               /**< @brief Length of the list */
 };
 
